cf36b6126008f01746362783bc8e542648e55f2e
j0sa <jonas.arucard@gmail.com>
2019-10-29 13:27:12 +0100

    create note as html

--- scripts/create.js
+++ scripts/create.js
@@ -1,7 +1,16 @@
+let array = [];
+
 let quill = new Quill('#editor', {
   theme: 'snow'
 });
 
 function saveNote() {
   console.log(quill.root.innerHTML);
+  array.push(quill.root.innerHTML);
+}
+
+function displayNote() {
+  array.forEach(element => {
+    document.getElementById('noteList').innerHTML = element;
+  });
 }
