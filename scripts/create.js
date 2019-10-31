92fece4788d081226f03d69735315ea9328da9f4
droidspeak <jonas.arucard@gmail.com>
2019-10-31 10:47:40 +0100

    added alert

--- scripts/create.js
+++ scripts/create.js
@@ -1,5 +1,6 @@
 let notes = [];
 
+var Delta = Quill.import('delta');
 let quill = new Quill('#editor', {
   theme: 'snow'
 });
@@ -28,3 +29,23 @@ window.onbeforeunload = function () {
     return 'There are unsaved changes. Are you sure you want to leave?';
   }
 }
+
+/*
+// Store accumulated changes
+let change = new Delta();
+quill.on('text-change', function (delta) {
+  change = change.compose(delta);
+});
+
+// Save periodically
+setInterval(function () {
+  if (change.length() > 0) {
+    console.log('Saving changes', change);
+    // Save the entire updated text to localStorage
+    const data = JSON.stringify(quill.getContents())
+    localStorage.setItem('data', data);
+    change = new Delta();
+  }
+}, 5 * 1000);
+
+*/
