692d7f255c16ed2f788421955b038df235573546
j0sa <jonas.arucard@gmail.com>
2019-11-04 12:53:04 +0100

    color changes, concatenations and other

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+/* Redirect code start page  */
+//<!-- redirect information page -->
+$(document).ready(function () {
+    if (typeof Cookies.get('secondvisit') === 'undefined') {
+        window.location.href = "about.html";
+    }
+})
+//<!-- Adds second Visit cookie -->
+  $(document).ready(function () {
+    // Create cookie so that the user is no longer redirected
+    let a = Cookies.set('secondvisit', 'true', {
+      expires: 7
+    });
+  });
