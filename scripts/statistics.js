956c978ff5779bc1e2862bacaa6a2c75e7c82c9a
j0sa <jonas.arucard@gmail.com>
2019-11-21 11:59:50 +0100

    hotfix

--- scripts/statistics.js
+++ scripts/statistics.js
@@ -160,7 +160,7 @@ function getStatsOnNotes() {
   let mostCommonWord = wordFreq[0][0];
   let leastCommonWord = wordFreq.slice(-2)[0][0];
   console.log(wordFreq);
-  return 'You have written ' + wordCnt + ' words. ' + uniqueWords + ' of them are unique.' + 'Your most common word is: ' + mostCommonWord + ' and your least common word is ' + leastCommonWord;
+  return 'You have written ' + wordCnt + ' words. ' + uniqueWords + ' of them are unique.' + ' Your most common word is ' + mostCommonWord + ' and your least common word is ' + leastCommonWord;
 }
 
 
